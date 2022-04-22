75351fb1d0e5b4c4b9f66fbe4c2a7a7f4930d68b
qsewell <103156726+qsewell@users.noreply.github.com>
2022-04-22 11:22:51 -0700

    4 Tests Implemented

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,51 @@
 import static org.junit.Assert.*;
+import java.nio.file.*;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 import org.junit.*;
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    static String getMarkdown(String fileName) throws IOException {
+        Path fileName2 = Path.of(fileName);
+        String content = Files.readString(fileName2);
+        return content;
+    }
+
+    @Test
+    public void MarkdownParseFileTester() throws IOException {
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("https://something.com");
+        arr.add("some-thing.html");
+        assertEquals(arr, MarkdownParse.getLinks(MarkdownParseTest.getMarkdown("test-file.md")));
+    }
+
+    @Test
+    public void MarkdownParseFileTester2() throws IOException {
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("https://something.com");
+        arr.add("some-thing.html");
+        assertEquals(arr, MarkdownParse.getLinks(MarkdownParseTest.getMarkdown("test-file-2.md")));
+    }
+
+    @Test
+    public void MarkdownParseFileTester3() throws IOException {
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("https://something.com");
+        arr.add("https://something.com");
+        arr.add("some-thing.html");
+        assertEquals(arr, MarkdownParse.getLinks(MarkdownParseTest.getMarkdown("test-file-3.md")));
+    }
+
+    @Test
+    public void MarkdownParseFileTester4() throws IOException {
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("https://google.com");
+        assertEquals(arr, MarkdownParse.getLinks(MarkdownParseTest.getMarkdown("test-file-4.md")));
     }
 }
--- test-file-4.md
+++ test-file-4.md
@@ -1,9 +1,2 @@
-[link1](https://something.com)
-# Title
 
-[link1](https://something.com)
-![Image](imagelinkhello)
-[link2](some-thing.html)
-
-[link2](some-thing.html
-![Image](imagelinkhello2)
+[Link](https://google.com)
